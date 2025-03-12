84b62015d955df8d435040e672ccb486a31d08fa
Umashankari <shankariu804@gmail.com>
2025-03-12 12:18:01 +0530

    Update Overview.cs

--- Pages/Overview.cs
+++ Pages/Overview.cs
@@ -1,32 +1,4 @@
-﻿//using OpenQA.Selenium;
-
-//namespace SwagProject.Pages
-//{
-//    internal class Overview
-//    {
-//        private readonly IWebDriver driver;
-
-//        public Overview(IWebDriver driver)
-//        {
-//            this.driver = driver;
-//        }
-
-//        // Locator
-//        private readonly By finishButton = By.XPath("//button[@id='finish']");
-
-//        public void ClickFinish()
-//        {
-//            driver.FindElement(finishButton).Click();
-//        }
-//    }
-//}
-
-
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using SwagProject.Locators;
@@ -35,26 +7,63 @@ namespace SwagProject.Pages
 {
     internal class Overview
     {
-        IWebDriver driver = SwagProject.Hooks.Hooks.driver;
+        private readonly IWebDriver driver;
 
-        /* public Overview()
-         {
+        // ✅ Constructor accepts WebDriver instance
+        public Overview(IWebDriver webDriver)
+        {
+            this.driver = webDriver;
+        }
 
-
-         }*/
-
-        //By finishButton = By.XPath("//button[@id='finish']");
-
-        public void finish()
+        public void Finish()
         {
             driver.FindElement(Complete.finishButton).Click();
             Thread.Sleep(1000);
-
         }
-        public void confirmationPage()
+
+        public void ConfirmationPage()
         {
             string actualText = driver.FindElement(Complete.confirmationMessage).Text;
             Assert.That(actualText, Is.EqualTo("Thank you for your order!"));
         }
     }
 }
+
+
+
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
+// using NUnit.Framework;
+// using OpenQA.Selenium;
+// using SwagProject.Locators;
+
+// namespace SwagProject.Pages
+// {
+//     internal class Overview
+//     {
+//         IWebDriver driver = SwagProject.Hooks.Hooks.driver;
+
+//         /* public Overview()
+//          {
+
+
+//          }*/
+
+//         //By finishButton = By.XPath("//button[@id='finish']");
+
+//         public void finish()
+//         {
+//             driver.FindElement(Complete.finishButton).Click();
+//             Thread.Sleep(1000);
+
+//         }
+//         public void confirmationPage()
+//         {
+//             string actualText = driver.FindElement(Complete.confirmationMessage).Text;
+//             Assert.That(actualText, Is.EqualTo("Thank you for your order!"));
+//         }
+//     }
+// }
